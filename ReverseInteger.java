a6311c3e3b153c099fde070b617e043f593d0446
VishnuRam16 <vrm6007@gmail.com>
2021-06-14 17:44:52 +0530

    Coded solution for Reverse Integer with respect to constraints

---
+++ ReverseInteger.java
@@ -0,0 +1,52 @@
+public class ReverseInteger 
+{
+  public static void reverse(int x) 
+    {
+        long rev = 0, rem;
+        
+        while(x != 0)
+        {
+            rem = x % 10;
+            rev = rev*10 + rem;
+            x /= 10;
+        }
+        
+        if ( rev < Integer.MIN_VALUE || rev > Integer.MAX_VALUE)
+        {
+        System.out.println(0);
+        }
+        else
+        {
+            System.out.println((int)rev);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        reverse(123);
+    }
+}
+
+//Solution that will be accepted by LeetCode:
+// class Solution {
+//   public int reverse(int x) 
+//   {
+//       long rev = 0, rem, temp = x; 
+      
+//       while(x != 0)
+//       {
+//           rem = x % 10;
+//           rev = rev*10 + rem;
+//           x /= 10;
+//       }
+      
+//       if ( rev < Integer.MIN_VALUE || rev > Integer.MAX_VALUE)
+//       {
+//       return 0;
+//       }
+//       else
+//       {
+//           return (int)rev;
+//       }
+//   }
+// }
