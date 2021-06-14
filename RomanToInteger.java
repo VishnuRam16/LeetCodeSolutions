45eb4a30aa6fca12e3052f50317aca4c7c3b3baf
VishnuRam16 <vrm6007@gmail.com>
2021-06-14 21:01:29 +0530

    Added code in comments that will be accepted by LeetCode

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -50,3 +50,52 @@ public class RomanToInteger
       romanToInt("III");
     }
 }
+
+//Code that will be accepted by LeetCode:
+// class Solution 
+// {
+//     public int romanToInt(String s) 
+//     {
+//         int result = 0;
+        
+//         s=s.replace("IV","IIII");
+//         s=s.replace("IX","IIIIIIIII");
+//         s=s.replace("XL","XXXX");
+//         s=s.replace("XC","XXXXXXXXX");
+//         s=s.replace("CD","CCCC");
+//         s=s.replace("CM","CCCCCCCCC");
+        
+//         for(int i=0;i<s.length();i++)
+//         {
+            
+//             switch(s.charAt(i))
+//             {
+//                 case 'I':
+//                     result+=1;
+//                     break;
+//                 case 'V':
+//                     result+=5;
+//                     break;
+//                 case 'X':
+//                     result+=10;
+//                     break;
+//                 case 'L':
+//                     result+=50;
+//                     break;
+//                 case 'C':
+//                     result+=100;
+//                     break;
+//                 case 'D':
+//                     result+=500;
+//                     break;
+//                 case 'M':
+//                     result+=1000;
+//                     break;
+                    
+//             }
+         
+//         }
+//         return result;
+    
+//     }
+// }
